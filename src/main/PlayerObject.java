4b2c7e0ef715909c29290c1c9adb72d7daaf57f8
Hans Bockgaard <hasseoa@gmail.com>
2017-10-17 11:18:27 +0200

    add playerObject.class

---
+++ src/main/PlayerObject.java
@@ -0,0 +1,48 @@
+package main;
+
+public class PlayerObject {
+
+    int xPos;
+    int yPos;
+    double xSpeed;
+    double ySpeed;
+
+
+    //region getters & setters
+
+    public int getxPos() {
+        return xPos;
+    }
+
+
+    public void setxPos(int xPos) {
+        this.xPos = xPos;
+    }
+
+    public int getyPos() {
+        return yPos;
+    }
+
+    public void setyPos(int yPos) {
+        this.yPos = yPos;
+    }
+
+    public double getxSpeed() {
+        return xSpeed;
+    }
+
+    public void setxSpeed(double xSpeed) {
+        this.xSpeed = xSpeed;
+    }
+
+    public double getySpeed() {
+        return ySpeed;
+    }
+
+    public void setySpeed(double ySpeed) {
+        this.ySpeed = ySpeed;
+    }
+    //endregion
+
+
+}//end of class
